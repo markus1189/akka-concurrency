2588bc3c08c9fff51c2aaf1ae4530bdcd250dad0
Markus Hauck <markus1189@gmail.com>
2013-05-19 15:03:06 +0200

    Add scalaz to build

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,8 @@ object AkkaBookBuild extends Build {
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       libraryDependencies ++= Seq( "org.scalatest" %% "scalatest" % "2.0.M5b" % "test"
                                  , "com.typesafe.akka" %% "akka-actor" % "2.1.0" 
-                                 , "com.typesafe.akka" %% "akka-testkit" % "2.1.0" 
+                                 , "com.typesafe.akka" %% "akka-testkit" % "2.1.0" % "test"
+                                 , "org.scalaz" %% "scalaz-core" % "7.0.0"
                                  )
     )
   )
